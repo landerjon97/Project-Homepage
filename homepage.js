3589b0d4c1bf7174d239efafee6427a392faeadb
wwrich <lander.jon9@gmail.com>
2017-11-03 21:59:54 -0400

    other

--- homepage.js
+++ homepage.js
@@ -27,9 +27,14 @@ function draw()
     if(makeOne== 1 && circles.length<= 150){
         circles.push(new DrawCircles());
     }
+    ctx.fillStyle="White";
     ctx.fillRect(0,0,can.width, can.height);
+    ctx.fillStyle="black";
+    ctx.fillRect(40,40,30,20);
+    for(var i = 0; i < circles.length; i++){
          if(circles[i].start >= can.height + circles[i].radius*2){
              circles.splice(i,1,new DrawCircles());
+             
          }
          else{
              circles[i].drawit();
